efa5fbd1e584b6154c3af4c350cf233a11609628
Ryan LeViseur <vidkun@users.noreply.github.com>
2017-03-13 10:35:41 -0500

    build out splunk searches without the sdk and only using basic REST api

--- misp_modules/modules/import_mod/splunkesimport.py
+++ misp_modules/modules/import_mod/splunkesimport.py
@@ -1,7 +1,9 @@
 import json
 import base64
-import splunklib.client
-import splunklib.results
+import urllib
+import httplib2
+from xml.dom import minidom
+import time
 
 misperrors = {'error': 'Error'}
 userConfig = {
@@ -46,26 +48,94 @@ moduleinfo = {'version': '0.1', 'author': 'Ryan LeViseur',
 moduleconfig = [ 'splunk_es_server', 'splunk_es_server_port' ]
 
 def handler(q=False):
-    if q is False:
-        return False
-    request = json.loads(q)
-    # For now just return request to get basic skel of module working
-    return request
+  if q is False:
+      return False
+  request = json.loads(q)
+  config = q.get("config", {})
+
+  baseurl = "https://{0}:{1}".format(config.get("splunk_es_server", None),
+                                     config.get("splunk_es_server_port", None))
+  userName = 'admin'
+  password = 'changeme'
+
+  # limit results to 10 while testing
+  searchQuery = '| `ip_intel` | head 10'
+
+  # Auth with Splunk server to get a session key
+  sessionKey = splunkLogin(userName, password, baseurl)
+
+  # Execute the Splunk search
+  searchId = splunkSearch(searchQuery, sessionKey, baseurl)
+
+  # Wait for search to complete
+  done = 0
+  while done is not 1:
+    done = getSplunkSearchStatus(searchId, sessionKey, baseurl)
+    # take a quick nap so we don't hammer the Splunk server
+    time.sleep(5)
+
+  # Pull down search results from Splunk server  
+  searchResults = getSplunkSearchResults(searchId, sessionKey, baseurl)
+
+  # For now just return request to get basic skel of module working
+  return request
 
 def introspection():
-    modulesetup = {}
-    try:
-        userConfig
-        modulesetup['userConfig'] = userConfig
-    except NameError:
-        pass
-    try:
-        inputSource
-        modulesetup['inputSource'] = inputSource
-    except NameError:
-        pass
-    return modulesetup
+  modulesetup = {}
+  try:
+      userConfig
+      modulesetup['userConfig'] = userConfig
+  except NameError:
+      pass
+  try:
+      inputSource
+      modulesetup['inputSource'] = inputSource
+  except NameError:
+      pass
+  return modulesetup
 
 def version():
-    moduleinfo['config'] = moduleconfig
-    return moduleinfo
+  moduleinfo['config'] = moduleconfig
+  return moduleinfo
+
+def splunkLogin(user, password, baseurl)
+  # Authenticate with server.
+  # Disable SSL cert validation. Splunk certs are self-signed.
+  serverContent = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/auth/login',
+      'POST', headers={}, body=urllib.urlencode({'username':userName, 'password':password}))[1]
+
+  return minidom.parseString(serverContent).getElementsByTagName('sessionKey')[0].childNodes[0].nodeValue
+
+def splunkSearch(searchQuery, sessionKey, baseurl)
+  # Remove leading and trailing whitespace from the search
+  searchQuery = searchQuery.strip()
+
+  # If the query doesn't already start with the 'search' operator or another 
+  # generating command (e.g. "| inputcsv"), then prepend "search " to it.
+  if not (searchQuery.startswith('search') or searchQuery.startswith("|")):
+      searchQuery = 'search ' + searchQuery
+
+  # Execute the search. Returns the search job ID.
+  searchJob = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/search/jobs','POST',
+  headers={'Authorization': 'Splunk %s' % sessionKey},body=urllib.urlencode({'search': searchQuery}))[1]
+
+  jobID = minidom.parseString(searchJob).getElementsByTagName('sid')[0].childNodes[0].nodeValue
+
+  return jobID
+
+def getSplunkSearchStatus(jobId, sessionKey, baseurl)
+  # Check the status of the search job
+  jobStatus = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/search/jobs/%s' % jobId,'GET',
+  headers={'Authorization': 'Splunk %s' % sessionKey},)[1]
+
+  jobInfo = minidom.parseString(jobStatus).getElementsByTagName('s:key')
+  for element in jobInfo:
+    if element.getAttribute('name') == "isDone":
+      return element.childNodes[0].nodeValue
+
+def getSplunkSearchResults(jobId, sessionKey, baseurl)
+  # Pull down the results of the search
+  results = httplib2.Http(disable_ssl_certificate_validation=True).request(baseurl + '/services/search/jobs/%s/results' % jobId,'GET',
+  headers={'Authorization': 'Splunk %s' % sessionKey},)[1]
+
+  return results
